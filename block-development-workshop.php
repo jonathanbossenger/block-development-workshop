f54162534da6c0a0ff2ff77ab58e817277324ac4
Jonathan Bossenger <jonathanbossenger@gmail.com>
2020-08-04 21:12:14 +0200

    Adding database table and form submission

--- block-development-workshop.php
+++ block-development-workshop.php
@@ -27,6 +27,24 @@ function bdw_error_log( $message, $data = '' ) {
 /**
  * Creates the custom table on plugin activation
  */
+register_activation_hook( __FILE__, 'bdw_create_table' );
+function bdw_create_table() {
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	global $wpdb;
+
+	$table_name   = $wpdb->prefix . 'subscribers';
+	$wpdb_collate = $wpdb->collate;
+	$sql          = "CREATE TABLE {$table_name} (
+		                id mediumint(11) unsigned NOT NULL auto_increment,
+		                status varchar(100) NULL,
+		                email varchar(100) NULL,
+		                name varchar(100) NULL,
+		                PRIMARY KEY  (id)
+		            )
+		            COLLATE {$wpdb_collate}";
+
+	dbDelta( $sql );
+}
 
 /**
  * A simple form shortcode
@@ -60,8 +78,8 @@ function bdw_form_shortcode() {
  * Processing the form data
  * https://developer.wordpress.org/reference/hooks/wp/
  */
-add_action( 'wp', 'bdw_maybe_process_form' );
-function bdw_maybe_process_form() {
+add_action( 'wp', 'bdw_process_form' );
+function bdw_process_form() {
 	//@todo homework: learn about and implement nonce checking
 	if ( ! isset( $_POST['bdw_form'] ) ) { //phpcs:ignore WordPress.Security
 		return;
@@ -70,10 +88,40 @@ function bdw_maybe_process_form() {
 	if ( ! empty( $bdw_form ) && 'submit' === $bdw_form ) {
 		$email          = $_POST['email']; //phpcs:ignore WordPress.Security.NonceVerification
 		$name           = $_POST['name']; //phpcs:ignore WordPress.Security.NonceVerification
-		$subscribe_data = array(
+		$subscriber_data = array(
 			'status'        => 'subscribed',
 			'name'          => $name,
 			'email_address' => $email,
 		);
+		$added = bdw_insert_subscriber( $subscriber_data );
 	}
+}
+
+/**
+ * Adds a subscriber to the database table
+ *
+ * @param $subscriber_data
+ *
+ * @return bool
+ */
+function bdw_insert_subscriber( $subscriber_data ) {
+	if ( empty( $subscriber_data['email'] ) ) {
+		return false;
+	}
+	global $wpdb;
+
+	$table_name = $wpdb->prefix . 'subscribers';
+
+	$rows = $wpdb->insert(
+		$table_name,
+		$subscriber_data
+	);
+
+	if ( 1 === $rows ) {
+		return true;
+	}
+
+	bdw_error_log( 'Failed adding subscriber to database', $subscriber_data );
+
+	return false;
 }
