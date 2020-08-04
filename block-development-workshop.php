22641ef1d40b56bc2bfee25cee95630e9b56cb46
Jonathan Bossenger <jonathanbossenger@gmail.com>
2020-08-04 20:49:42 +0200

    Initial commit

---
+++ block-development-workshop.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Plugin Name:     Block Development Workshop
+ * Plugin URI:      https://jonathanbossenger.com
+ * Description:     Take an existing plugin, and add some blocks
+ * Author:          Jonathan Bossenger
+ * Author URI:      https://jonathanbossenger.com
+ * Text Domain:     block-development-workshop
+ * Domain Path:     /languages
+ * Version:         0.0.1
+ *
+ * @package         Block_Development_Workshop
+ */
+
+function bdw_error_log( $message, $data = '' ) {
+	$file = trailingslashit( plugin_dir_path( __FILE__ ) ) . 'log/' . date( 'Y-m-d' ) . '.log';
+	if ( ! is_file( $file ) ) {
+		file_put_contents( $file, '' );
+	}
+	if ( ! empty( $data ) ) {
+		$message = array( $message => $data );
+	}
+	$data_string = print_r( $message, true ) . "\n";
+	file_put_contents( $file, $data_string, FILE_APPEND );
+}
+
+/**
+ * Creates the custom table on plugin activation
+ */
+
+/**
+ * A simple form shortcode
+ * https://developer.wordpress.org/reference/functions/add_shortcode/
+ */
+add_shortcode( 'bdw_form_shortcode', 'bdw_form_shortcode' );
+function bdw_form_shortcode() {
+	ob_start();
+	?>
+	<h1>Subscribe</h1>
+	<form method="post">
+		<input type="hidden" name="bdw_form" value="submit">
+		<div>
+			<label for="email">Email address</label>
+			<input type="text" id="email" name="email" placeholder="Email address">
+		</div>
+		<div>
+			<label for="name">Name</label>
+			<input type="text" id="name" name="name" placeholder="Name">
+		</div>
+		<div>
+			<input type="submit" id="submit" name="submit" value="Submit">
+		</div>
+	</form>
+	<?php
+	$form = ob_get_clean();
+	return $form;
+}
+
+/**
+ * Processing the form data
+ * https://developer.wordpress.org/reference/hooks/wp/
+ */
+add_action( 'wp', 'bdw_maybe_process_form' );
+function bdw_maybe_process_form() {
+	//@todo homework: learn about and implement nonce checking
+	if ( ! isset( $_POST['bdw_form'] ) ) { //phpcs:ignore WordPress.Security
+		return;
+	}
+	$bdw_form = $_POST['bdw_form']; //phpcs:ignore WordPress.Security.NonceVerification
+	if ( ! empty( $bdw_form ) && 'submit' === $bdw_form ) {
+		$email          = $_POST['email']; //phpcs:ignore WordPress.Security.NonceVerification
+		$name           = $_POST['name']; //phpcs:ignore WordPress.Security.NonceVerification
+		$subscribe_data = array(
+			'status'        => 'subscribed',
+			'name'          => $name,
+			'email_address' => $email,
+		);
+	}
+}
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Block_Development_Workshop
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
